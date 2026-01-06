008372c4bfed1549a60ce9b4434010d68dfbef34
Donald Se <donaldse175@gmail.com>
2026-01-06 18:43:20 +0100

    Modification du header au propre

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Import des styles globaux
+import App from './App.jsx'
+import './index.css'
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+
+            staleTime: 1000 * 60 * 5,
+            refetchOnWindowFocus: false,
+        },
+    },
+})
+
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+    throw new Error("ERREUR CRITIQUE : L'élément <div id='root'> est introuvable dans index.html.")
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
       <BrowserRouter>
           <App />
       </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
